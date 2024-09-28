52af7708f6dc3f1ade5933f65ca9cda1b377337f
Ashwin1108 <75871066+Ashwin1108@users.noreply.github.com>
2024-09-28 19:37:52 +0530

    add refreshAccessToken endpoint

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,7 +55,10 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
-userSchema.method.isPasswordCorrect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function (password) {
+  if (!password || !this.password) {
+    throw new Error("Missing password arguments for comparison.");
+  }
   return await bcrypt.compare(password, this.password);
 };
 
